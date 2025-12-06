16240958f5dd581d06ef7a9be7cfbf27b3736862
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-12-06 15:53:09 +0000

    Add register op

--- vir_py_rs_type/src/op.rs
+++ vir_py_rs_type/src/op.rs
@@ -3,16 +3,42 @@ use crate::builtin::{VirPyFloat, VirPyInt};
 use bumpalo::Bump;
 use std::ops::Add;
 
+
 type OpFn = for<'ctx> fn(
     lhs: Value<'ctx>,
     rhs: Value<'ctx>,
     arena: &'ctx Bump,
 ) -> Option<Value<'ctx>>;
 
-pub struct OpAddImpl {
-    pub function: OpFn,
+#[macro_export]
+macro_rules! __register_op {
+    (
+        $lhs_type:ty,
+        $rhs_type:ty,
+        $func:expr,
+        $output_wrapper:path,
+        $impl_path:path
+    ) => {
+        const _: () = {
+            fn _op_impl<'ctx>(
+                lhs: $crate::base::Value<'ctx>,
+                rhs: $crate::base::Value<'ctx>,
+                arena: &'ctx ::bumpalo::Bump,
+            ) -> Option<$crate::base::Value<'ctx>> {
+                let lhs_val = <$lhs_type as $crate::base::Downcast>::from_value(lhs)?;
+                let rhs_val = <$rhs_type as $crate::base::Downcast>::from_value(rhs)?;
+                let result = $func(lhs_val.clone(), rhs_val.clone());
+                Some($crate::base::ValueContainer::new($output_wrapper(result), arena))
+            }
+
+            ::inventory::submit! {
+                $impl_path { function: _op_impl }
+            }
+        };
+    };
 }
 
+pub struct OpAddImpl { pub function: OpFn }
 inventory::collect!(OpAddImpl);
 
 pub fn op_add<'ctx>(lhs: Value<'ctx>, rhs: Value<'ctx>, arena: &'ctx Bump) -> Option<Value<'ctx>> {
@@ -26,25 +52,16 @@ pub fn op_add<'ctx>(lhs: Value<'ctx>, rhs: Value<'ctx>, arena: &'ctx Bump) -> Op
 
 #[macro_export]
 macro_rules! register_op_add {
-    (
-        $lhs_type:ty,
-        $rhs_type:ty,
-        $output_wrapper:path
-    ) => {
-        const _: () = {
-            fn op_impl_fn<'ctx>(
-                lhs: $crate::base::Value<'ctx>,
-                rhs: $crate::base::Value<'ctx>,
-                arena: &'ctx ::bumpalo::Bump,
-            ) -> Option<$crate::base::Value<'ctx>> {
-                let lhs_val = <$lhs_type as $crate::base::Downcast>::from_value(lhs)?;
-                let rhs_val = <$rhs_type as $crate::base::Downcast>::from_value(rhs)?;
-                let result = lhs_val.clone() + rhs_val.clone();
-                Some($crate::base::ValueContainer::new($output_wrapper(result), arena))
-            }
-            ::inventory::submit! {
-                $crate::op::OpAddImpl { function: op_impl_fn }
-            }
-        };
+    ($lhs_type:ty, $rhs_type:ty, $output_wrapper:path) => {
+        $crate::__register_op!(
+            $lhs_type,
+            $rhs_type,
+            |a, b| a + b,
+            $output_wrapper,
+            $crate::op::OpAddImpl
+        );
     };
 }
+
+register_op_add!(VirPyInt,   VirPyInt,   ValueKind::Int);
+register_op_add!(VirPyFloat, VirPyFloat, ValueKind::Float);
