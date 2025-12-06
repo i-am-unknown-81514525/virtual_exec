24d8ceeb6de6732a34cd0cb481c2e56dd664c599
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-12-06 21:38:48 +0000

    Add unary operator

--- vir_py_rs_type/src/op.rs
+++ vir_py_rs_type/src/op.rs
@@ -4,6 +4,9 @@ use bumpalo::Bump;
 type BinaryOpFn =
     for<'ctx> fn(lhs: Value<'ctx>, rhs: Value<'ctx>, arena: &'ctx Bump) -> Option<Value<'ctx>>;
 
+type UnaryOpFn =
+    for<'ctx> fn(rhs: Value<'ctx>, arena: &'ctx Bump) -> Option<Value<'ctx>>;
+
 #[macro_export]
 macro_rules! __binary_op_register {
     (
@@ -73,6 +76,73 @@ macro_rules! __binary_op_create {
     };
 }
 
+
+#[macro_export]
+macro_rules! __unary_op_register {
+    (
+        $rhs_type:ty,
+        $func:expr,
+        $output_wrapper:path,
+        $impl_path:path
+    ) => {
+        const _: () = {
+            fn _op_impl<'ctx>(
+                rhs: $crate::base::Value<'ctx>,
+                arena: &'ctx ::bumpalo::Bump,
+            ) -> Option<$crate::base::Value<'ctx>> {
+                let rhs_val = <$rhs_type as $crate::base::Downcast>::from_value(rhs)?;
+                match $func(rhs_val.clone()) {
+                    Ok(result) => {
+                        Some($crate::base::ValueContainer::new(
+                            $output_wrapper(result),
+                            arena,
+                        ))
+                    }
+                    Err(err) => {
+                        Some($crate::base::ValueContainer::new(
+                            $crate::base::ValueKind::ErrorWrapped(err),
+                            arena,
+                        ))
+                    }
+                }
+            }
+
+            ::inventory::submit! {
+                $impl_path { function: _op_impl }
+            };
+        };
+    };
+}
+
+macro_rules! __unary_op_create {
+    ($name:tt, $alt_name:tt, $op:tt) => {
+        __unary_op_create!(@impl $name, @impl $alt_name, $op, $);
+    };
+    (@impl $name:tt, @impl $alt_name:tt, $op:tt, $d:tt) => {
+        ::paste::paste!{
+            pub struct [< Op $alt_name Impl>] {pub function: $crate::op::UnaryOpFn }
+            ::inventory::collect!([< Op $alt_name Impl>]);
+            pub fn [< op_ $name>]<'ctx>(rhs: $crate::base::Value<'ctx>, arena: &'ctx ::bumpalo::Bump) -> ::core::option::Option<$crate::base::Value<'ctx>> {
+                for implementation in ::inventory::iter::<[<Op $alt_name Impl>]> {
+                    if let ::core::option::Option::Some(result) = (implementation.function)(rhs, arena) {
+                        return Some(result);
+                    }
+                }
+                None
+            }
+            #[macro_export]
+            macro_rules! [<register_op_ $name>] {
+                ($d rhs_type:ty, $d output_wrapper:path) => {
+                    [<register_op_ $name>]!($d rhs_type, $d output_wrapper, |a, b| a $op b);
+                };
+                ($d rhs_type:ty, $d output_wrapper:path, $d func:expr) => {
+                    $crate::__unary_op_register!($d rhs_type, $d func, $d output_wrapper, $crate::op::[<Op $alt_name Impl>]);
+                }
+            }
+        }
+    };
+}
+
 __binary_op_create!(add, Add, +);
 __binary_op_create!(sub, Sub, -);
 __binary_op_create!(mul, Mul, *);
@@ -89,4 +159,7 @@ __binary_op_create!(bsr, Bsr, >>);
 __binary_op_create!(band, BitwiseAnd, &);
 __binary_op_create!(bor, BitwiseOr, |);
 __binary_op_create!(bxor, BitwiseXor, ^);
-// __op_create!(not, Not, !); // Not a binary operation
+__unary_op_create!(not, Not, !);
+__unary_op_create!(pos, Pos, +);
+__unary_op_create!(neg, Neg, -);
+__unary_op_create!(bnot, BitwiseNot, ~);
