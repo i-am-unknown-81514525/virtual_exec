31e51bbe67d1ebe2de365b52443d2ea44d1d3177
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-12-07 19:59:40 +0000

    Add even more test

--- vir_py_rs_macro/tests/macro_test.rs
+++ vir_py_rs_macro/tests/macro_test.rs
@@ -78,3 +78,74 @@ fn test_more() {
         _ => panic!("Expected an integer result, but got {:?}", value),
     }
 }
+
+#[test]
+fn test_timeout() {
+    let module = parse!({
+        a = 10;
+        a = a + 5;
+        if a == 15 {
+            a = 2;
+        }
+        a;
+    });
+    let arena_rc = Rc::new(RefCell::new(Bump::new()));
+    let mut global_scope = Mapping { mapping: HashMap::new() };
+
+    let initial_value: Value<'static> = {
+        let arena_borrow: Ref<Bump> = arena_rc.borrow();
+        let arena_ref: &Bump = &arena_borrow;
+        let long_lived_arena: &'static Bump = unsafe { std::mem::transmute(arena_ref) };
+        ValueContainer::new(ValueKind::None, long_lived_arena)
+    };
+
+    global_scope.mapping.insert("a".to_string(), Rc::new(RefCell::new(initial_value)));
+
+    let mapping = vec![Rc::new(RefCell::new(global_scope))];
+    let ctx = Rc::new(RefCell::new(ExecutionContext::new(arena_rc.clone(), 15, mapping.clone())));
+
+    let result = module.eval(ctx);
+
+    assert!((&result).is_err(), "Evaluation successful when TimeoutError is expected: {:?}", result.ok());
+    assert!(match (result.clone().err()) {
+        Some(vir_py_rs_type::error::SandboxExecutionError::TimeoutError) => true,
+        _ => false
+    }, "Expected TimeoutError, but got {:?}", result.err());
+}
+
+#[test]
+fn test_if_fail_path() {
+    let module = parse!({
+        a = 10;
+        a = a + 5;
+        if a == 14 {
+            a = 2;
+        }
+        a;
+    });
+    let arena_rc = Rc::new(RefCell::new(Bump::new()));
+    let mut global_scope = Mapping { mapping: HashMap::new() };
+
+    let initial_value: Value<'static> = {
+        let arena_borrow: Ref<Bump> = arena_rc.borrow();
+        let arena_ref: &Bump = &arena_borrow;
+        let long_lived_arena: &'static Bump = unsafe { std::mem::transmute(arena_ref) };
+        ValueContainer::new(ValueKind::None, long_lived_arena)
+    };
+
+    global_scope.mapping.insert("a".to_string(), Rc::new(RefCell::new(initial_value)));
+
+    let mapping = vec![Rc::new(RefCell::new(global_scope))];
+    let ctx = Rc::new(RefCell::new(ExecutionContext::new(arena_rc.clone(), 1000, mapping.clone())));
+
+    let result = module.eval(ctx);
+
+    assert!(result.is_ok(), "Evaluation failed: {:?}", result.err());
+
+    let value = (&mapping).get(0).unwrap().borrow().mapping.get("a").unwrap().borrow().kind.clone();
+
+    match value {
+        ValueKind::Int(i) => assert_eq!(i.value, 15),
+        _ => panic!("Expected an integer result, but got {:?}", value),
+    }
+}
--- vir_py_rs_type/src/lib.rs
+++ vir_py_rs_type/src/lib.rs
@@ -4,7 +4,7 @@ pub mod op;
 pub mod ast;
 pub mod base;
 pub mod builtin;
-mod error;
+pub mod error;
 pub mod exec_ctx;
 pub mod export;
 mod op_impl;
